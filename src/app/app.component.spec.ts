5e47501e56156cb6e308af39c7a3dae3d0cdb553
TonyGregg <antonygenil@gmail.com>
2020-03-27 08:31:55 -0500

    Minor changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
-import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
-
 import { AppComponent } from './app.component';
-import { NavigationModule } from './main-layout/navigation/navigation.module';
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        NavigationModule,
         FormsModule,
         RouterTestingModule
       ],
